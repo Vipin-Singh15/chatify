c1f9450507719c43a45e2373e54c033b336e1bba
Vipin-Singh15 <vipinsingh7315@gmail.com>
2025-09-18 00:44:31 +0530

    welcome email implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,12 @@ import authRoutes from './routes/authRoutes.js'
 import messageRoutes from './routes/messageRoutes.js'
 import path from 'path'
 import { connectDB } from './lib/db.js'
+import { ENV } from './lib/env.js'
 
-dotenv.config()
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); //req.body
 
@@ -17,7 +17,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
 // make ready for deployment
-if (process.env.NODE_ENV == "production") {
+if (ENV.NODE_ENV == "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
     app.get("*", (_, res) => {
