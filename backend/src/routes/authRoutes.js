fb9e949e0ad36ba99a18893b5b7bd5502f41a3a0
Vipin-Singh15 <vipinsingh7315@gmail.com>
2025-09-21 04:50:53 +0530

    added update profile endpoint

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { signup ,login , logout} from "../controllers/authController.js";
+import { signup, login, logout, updateProfile } from "../controllers/authController.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.post("/signup", signup)
 router.post("/login", login)
 
 router.post("/logout", logout)
+router.put("/update-profile", protectRoute, updateProfile)  // protectRoute, a middleware here only update if it's authenticated, first protectRoute function will be called
 
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user)) // to authenticate
 export default router;
