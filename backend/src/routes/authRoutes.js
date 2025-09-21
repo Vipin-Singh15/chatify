c6ea9ac0c50d606350ade2c72039c36b15611532
Vipin-Singh15 <vipinsingh7315@gmail.com>
2025-09-21 20:43:30 +0530

    rate limiting implemented

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controllers/authController.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection);
 router.post("/signup", signup)
-
 router.post("/login", login)
-
 router.post("/logout", logout)
 router.put("/update-profile", protectRoute, updateProfile)  // protectRoute, a middleware here only update if it's authenticated, first protectRoute function will be called
 
