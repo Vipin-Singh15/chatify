eeb0a9aab603654285443f5dbe272d38f37d1393
Vipin-Singh15 <vipinsingh7315@gmail.com>
2025-09-28 03:53:10 +0530

    login implemented

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore';
 import BorderAnimatedContainer from '../components/BorderAnimated';
 import { MessageCircleIcon, LockIcon, MailIcon, UserIcon, LoaderIcon } from 'lucide-react';
